ada921b409ecece589336eb050a9e459f2bd5b82
karell606 <karell.azabe@epitech.digital>
2024-10-01 11:07:12 +0200

    ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,18 @@
+function changebackground() {
+  	var magicNumber = 1;
+    
+	 if (magicNumber === 1) { 
+   paintRedRectangle();
+ }
+ 
+ if (magicNumber===1) {
+ 	let magicNumber=2;
+  paintGreenRectangle();
+}
+  
+  if (magicNumber ===1 ){
+  	let magicNumber = 3;
+paintYellowRectangle();
+  }
+ }
+changebackground();
